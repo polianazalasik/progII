42c94469c808e073fade8ea8ad2b7f3940f03c15
polianazalasik <polianaazalasik@outlook.com>
2019-03-28 09:27:59 -0300

    Novas atualizacoes

--- TicoTico/comeco.py
+++ TicoTico/comeco.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template, request
 from pessoa import Pessoa
 
+lista_global = []
+
 app = Flask(__name__)
-lista = [Pessoa("Maria", "18")]
+
 
 @app.route("/")
 def listar_pessoas():
-    nome = request.args.get("nome")
-    idade = request.args.get("idade")
-    lista.append(Pessoa(nome, idade))
-    return render_template("listar_pessoas.html", pessoas = lista)
+    return render_template("listar_pessoas.html", pessoas = lista_global)
 
 @app.route("/form_inserir_pessoas")
-def inserir_pessoas():
+def mostrar_pessoas():
     return render_template("form_inserir_pessoas.html")
+
+@app.route("/cadastrar")
+def cadastrar_pessoas():
+    nome = request.args.get("nome")
+    idade = request.args.get("idade")
+    lista_global.append(Pessoa(nome, idade))
+    return listar_pessoas()
+
+@app.route("/excluir_pessoas")
+def excluir():
+    for i in lista_global:
+        x = request.args.get("nome")
+
 
 app.run(host="0.0.0.0")
--- TicoTico/pessoa.py
+++ TicoTico/pessoa.py
@@ -2,4 +2,3 @@ class Pessoa():
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
-        
