7757b54e6ab5e1157789f4499b5ff0d3913632dd
polianazalasik <47854859+polianazalasik@users.noreply.github.com>
2019-03-31 20:33:22 -0300

    Delete lista2fun4.py

--- lista2fun4.py
+++
@@ -1,9 +0,0 @@
-def par_ou_impar(numero):
-    if numero%2==0:
-        n = "par"
-    else:
-        n = "ímpar"
-    return n
-
-numero = int(input("Digite um número inteiro: "))
-print("O número é ", par_ou_impar(numero))
